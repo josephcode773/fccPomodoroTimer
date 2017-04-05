fc14638ac611fdbc7edb98eb1963bfb0b89b5e11
josephcode773 <josephcode773@gmail.com>
2017-04-04 20:27:12 -0500

    Added RESET button

--- script.js
+++ script.js
@@ -46,3 +46,7 @@ function millisToMinutesAndSeconds(millis) {
     var seconds = ((millis % 60000) / 1000).toFixed(0);
     return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
+
+function resetTimer() {
+    location.reload(false);
+}
