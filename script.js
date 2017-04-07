7c853470a2cf6173f30e34d326492b1f3e253d36
josephcode773 <josephcode773@gmail.com>
2017-04-06 21:57:09 -0500

    working.  needs way to customize length of each pomodoro

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
+var timerNumber = 30000;
+
 var bar = new ProgressBar.Circle(container, {
     color: '#aaa',
     // This has to be the same size as the maximum width to
     // prevent clipping
     strokeWidth: 4,
     trailWidth: 1,
-    easing: 'easeInOut',
-    duration: 5000,
+    easing: '',
+    duration: timerNumber,
     text: {
-        value: millisToMinutesAndSeconds(5000),
+        value: millisToMinutesAndSeconds(timerNumber),
         autoStyleContainer: false
     },
-    from: { color: '#aaa', width: 1 },
+    from: { color: '#aaa', width: 2 },
     to: { color: '#333', width: 4 },
     // Set default step function for all animate calls
     step: function(state, circle) {
         circle.path.setAttribute('stroke', state.color);
         circle.path.setAttribute('stroke-width', state.width);
 
-        var value = Math.round(circle.value() * 5000);
+        var value = Math.round(circle.value() * 100);
         if (value === 0) {
             circle.setText('');
         } else {
-            circle.setText(millisToMinutesAndSeconds(value));
+            circle.setText((value) + ' %');
+            console.log(tickerToMinutesAndSeconds(circle.value()));
         }
 
     }
@@ -29,6 +32,10 @@ var bar = new ProgressBar.Circle(container, {
 
 bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
 bar.text.style.fontSize = '2rem';
+
+
+// bar.duration = timerNumber;
+// bar.text.value = millisToMinutesAndSeconds(timerNumber);
 
 function barAnimate() {
     bar.animate(1.0, function () {
@@ -44,6 +51,12 @@ function barStop() {
 function millisToMinutesAndSeconds(millis) {
     var minutes = Math.floor(millis / 60000);
     var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+function tickerToMinutesAndSeconds(time) {
+    var minutes = Math.floor(time / 10);
+    var seconds = ((time % 60000) * 100).toFixed(0);
     return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
 
